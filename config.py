8fe9fc5837d3e7af901a2afc1ac9fbdce4500849
klaudiakantor <32897461+klaudiakantor@users.noreply.github.com>
2018-09-13 22:27:27 +0200

    webapp added

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+from dotenv import load_dotenv
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config(object):
+    def __init__(self):
+        env_path = os.path.join(os.getcwd(), '.env')
+        load_dotenv(dotenv_path=env_path)
+
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+    DATABASE_USER = os.getenv("DATABASE_USER")
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + DATABASE_USER + ':' + DATABASE_PASSWORD + '@localhost:5432/flights'
--- models.py
+++ models.py
@@ -1,33 +1,28 @@
 from app import db
-from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy import String, Column, Integer
 from datetime import datetime
 
-class Flights(db.Model):
 
-    __tablename__ = 'flight_table'
+class Flights(db.Model):
+    __tablename__ = 'flights'
     id = Column(Integer, primary_key=True)
     carrier = Column('carrier', String())
     created_at = Column(String, default=datetime.now().strftime("%Y-%m-%d"))
     date = Column('date', String)
     start = Column('start', String())
     stop = Column('stop', String())
-    #śflight_number = Column('flight_number', String())
     start_time = Column('start_time', String())
     stop_time = Column('end_time', String())
     price = Column('price', String())
 
-
-    def __init__(self, carrier, date, start, stop, start_time, stop_time, price):
+    def __init__(self, date, start, stop, start_time, stop_time, price, carrier):
         self.carrier = carrier
         self.date = date
         self.start = start
         self.stop = stop
-        #self.flight_number = flight_number
         self.start_time = start_time
         self.stop_time = stop_time
         self.price = price
-
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
